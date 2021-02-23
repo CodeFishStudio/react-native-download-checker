73da0e08b1995d7666d334f3a9a9b2bebdfc31f9
Will Turner <will@codefishstudio.com>
2021-02-23 10:48:29 +1030

    modify android context

--- android/src/main/java/org/codefishstudio/rndownloadchecker/RNDownloadCheckerModule.java
+++ android/src/main/java/org/codefishstudio/rndownloadchecker/RNDownloadCheckerModule.java
@@ -1,19 +1,8 @@
 package org.codefishstudio.rndownloadchecker;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.os.Build;
-import android.provider.MediaStore;
-
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeArray;
-import com.facebook.react.bridge.WritableNativeMap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -37,12 +26,12 @@ public class RNDownloadCheckerModule extends ReactContextBaseJavaModule {
 
 
     @ReactMethod
-    public boolean verifyInstallerId(Context context) {
+    public boolean verifyInstallerId() {
         // A list with valid installers package name
         List<String> validInstallers = new ArrayList<>(Arrays.asList("com.android.vending", "com.google.android.feedback"));
 
         // The package name of the app that has installed your app
-        final String installer = context.getPackageManager().getInstallerPackageName(context.getPackageName());
+        final String installer = reactContext.getPackageManager().getInstallerPackageName(reactContext.getPackageName());
 
         // true if your app has been downloaded from Play Store
         return installer != null && validInstallers.contains(installer);
